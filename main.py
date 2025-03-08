9cebe5410f267e1b67ff4b993ad8e0c9623f30c8
Sonu-2003 <swarnava2003.sonu@gmail.com>
2025-03-08 15:06:23 +0530

    8/3/2025

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import pandas as pd
 from PIL import Image
 import datetime
 import time
-
+#mpl_finance , candle_stick_ohlc
 
 #Checking if my directory is present or not, if not I will allocate one
 def assure_path_exists(path):
@@ -18,6 +18,7 @@ def assure_path_exists(path):
         os.makedirs(d)
 
 #1:07:67 Tuesday: This implements The real time clock to my system and it will update after 100 milliseconds
+#Reinforcement Learning- RL
 def tick():
     time_string= time.strftime('%H:%M:%S')
     clock.config(text=time_string)
@@ -42,7 +43,7 @@ def check_haarscas():
 #I will be showing you how to scan and test whether my image is registered in the database. If it's not, it will prompt the admin for changing password
 def save_pass():
     assure_path_exists("TrainingImageLabel/")
-    existing=os.path.isFile("TrainingImageLabel\password.txt")
+    existing=os.path.isfile("TrainingImageLabel\password.txt")
     if existing:
         tf=open("TrainingImageLabel\password.txt","r")
         key=tf.read()
@@ -56,5 +57,34 @@ def save_pass():
             tf.write(new_pass)
             mess._show(title='New Password Registered', message="Hey!!New Password registered Successfully!!")
             return
+    op=(old.get())
+    newp=(new.get())
+    nnewp=(nnew.get())
+    if(op==key):
+        if(newp==nnewp):
+            txf=open("TrainingImageLabel\password.txt","w")
+            txf.write(newp)
+        else:
+            mess._show(title="Error", message="Hey! Confirm me the new password")
+            return
+    else:
+        mess._show(title="Wrong Password", message="Please enter your correct password")
+        return
+    mess._show(title="Password changed", message="Hey! Password changed successfully")
+    master.destroy()
+    
+    #In the above code, we are actually Trying to set up a new password and check if the new password is matching with the new password comfirmation.abs
+def change_pass():
+    global m
+    m=tk.Tk()
+    m.geometry("300x160")
+    m.resizeable(False,False)
+    m.title("Change Password")
+    m.configure(background="white")
+    lbl=tk.Label(m,text=' Enter Old Password  ', bg='white', font=('times',12,'bold'))
+    lbl.pack()
+    global old
+    old=tk.Entry(m,width="25", fg="black", relief="solid", font=('times',12,'bold'),show="*")
+    
         
         
