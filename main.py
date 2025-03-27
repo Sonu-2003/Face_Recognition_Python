6c8624b95a1b8b93d43dae2738a6a175cf58a5f3
Sonu-2003 <swarnava2003.sonu@gmail.com>
2025-03-27 12:35:03 +0530

    Final Face Code

--- main.py
+++ main.py
@@ -148,17 +148,16 @@ def TakeImages():
     assure_path_exists("StudentDetails/")
     assure_path_exists("TrainingImage/")
     serial = 0
-    exists = os.path.isfile("StudentDetails/StudentDetails.csv")
+    exists = os.path.isfile("StudentDetails\StudentDetails.csv")
     if exists:
-        with open("StudentDetails/StudentDetails.csv", 'r') as csvFile1:
+        with open("StudentDetails\StudentDetails.csv", 'r') as csvFile1:
             reader1 = csv.reader(csvFile1)
-            #This will automatically run the code and allocate serial numbers from 1 to number of candidates
             for l in reader1:
                 serial = serial + 1
         serial = (serial // 2)
         csvFile1.close()
     else:
-        with open("StudentDetails/StudentDetails.csv", 'a+') as csvFile1:
+        with open("StudentDetails\StudentDetails.csv", 'a+') as csvFile1:
             writer = csv.writer(csvFile1)
             writer.writerow(columns)
             serial = 1
@@ -173,13 +172,13 @@ def TakeImages():
         while (True):
             ret, img = cam.read()
             gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            faces = detector.detectMultiScale(gray, 1.3, 6) #KNN- K nearest Neighbours. Just make surr value is within 3 to 7. Apart from this it is not a human face according to science.
+            faces = detector.detectMultiScale(gray, 1.3, 5)
             for (x, y, w, h) in faces:
                 cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
                 # incrementing sample number
                 sampleNum = sampleNum + 1
                 # saving the captured face in the dataset folder TrainingImage
-                cv2.imwrite("TrainingImage/ " + name + "." + str(serial) + "." + Id + '.' + str(sampleNum) + ".jpg",
+                cv2.imwrite("TrainingImage\ " + name + "." + str(serial) + "." + Id + '.' + str(sampleNum) + ".jpg",
                             gray[y:y + h, x:x + w])
                 # display the frame
                 cv2.imshow('Taking Images', img)
@@ -193,7 +192,7 @@ def TakeImages():
         cv2.destroyAllWindows()
         res = "Images Taken for ID : " + Id
         row = [serial, '', Id, '', name]
-        with open('StudentDetails/StudentDetails.csv', 'a+') as csvFile:
+        with open('StudentDetails\StudentDetails.csv', 'a+') as csvFile:
             writer = csv.writer(csvFile)
             writer.writerow(row)
         csvFile.close()
@@ -203,119 +202,128 @@ def TakeImages():
             res = "Enter Correct name"
             message.configure(text=res)
 
+
 def TrainImages():
     check_haarscas()
     assure_path_exists("TrainingImageLabel/")
-    recognizer=cv2.face_LBPHFaceRecognizer.create()
-    harcascadePath= "haarcascade_frontface_default.xml"
+    recognizer = cv2.face_LBPHFaceRecognizer.create()
+    harcascadePath = "haarcascade_frontalface_default.xml"
     detector = cv2.CascadeClassifier(harcascadePath)
-    faces, ID= getImagesAndLabels("TrainingImage")
-    
+    faces, ID = getImagesAndLabels("TrainingImage")
     try:
-        recognizer.train(faces,np.array(ID))
+        recognizer.train(faces, np.array(ID))
     except:
-        mess._show(title="No Registrations", message="Please Register Someone First!!")
+        mess._show(title='No Registrations', message='Please Register someone first!!!')
         return
-    recognizer.save("TrainingImageLabel/")
-    res="Profile Saved Successfully"
+    recognizer.save("TrainingImageLabel\Trainner.yml")
+    res = "Profile Saved Successfully"
     message1.configure(text=res)
-    message.configure(text="Total Registrations till now: "+ str(ID[0]))
-    
+    message.configure(text='Total Registrations till now  : ' + str(ID[0]))
+
+
 def getImagesAndLabels(path):
-    imagePaths=[os.path.join(path,f) for f in os.listdir(path)]
-    
-    #creating  empty lists
-    faces=[]
-    Ids=[]
-    
-    #We will loop through the image paths and load the Ids and their corresponding Faces, Eg. 1) Walaa ; 2) Swarnava
+    # get the path of all the files in the folder
+    imagePaths = [os.path.join(path, f) for f in os.listdir(path)]
+    # create empth face list
+    faces = []
+    # create empty ID list
+    Ids = []
+    # now looping through all the image paths and loading the Ids and the images
     for imagePath in imagePaths:
-        #load the face in the computer and convert it to gray scale
-        pilImage= Image.open(imagePath).convert('L')
-        
-        #Now we will convert PIL image into numpy array
-        imageNp=np.array(pilImage, 'uint32')
-        
-        #uint8- Unique 8 Integers
-        
+        # loading the image and converting it to gray scale
+        pilImage = Image.open(imagePath).convert('L')
+        # Now we are converting the PIL image into numpy array
+        imageNp = np.array(pilImage, 'uint8')
+        # getting the Id from the image
+        ID = int(os.path.split(imagePath)[-1].split(".")[1])
+        # extract the face from the training image sample
+        faces.append(imageNp)
+        Ids.append(ID)
+    return faces, Ids
+
+
+
 def TrackImages():
     check_haarscas()
     assure_path_exists("Attendance/")
     assure_path_exists("StudentDetails/")
     for k in tv.get_children():
         tv.delete(k)
-    msg=''
-    i=0
-    j=0
-    recognizer=cv2.face.LBPHFaceRecognizer_create()
-    exists3=os.path.isfile("TrainingImageLabel/Trainner.yml")
+    msg = ''
+    i = 0
+    j = 0
+    recognizer = cv2.face.LBPHFaceRecognizer_create()  # cv2.createLBPHFaceRecognizer()
+    exists3 = os.path.isfile("TrainingImageLabel\Trainner.yml")
     if exists3:
-        recognizer.read("TrainingImageLabel/Trainner.yml")
+        recognizer.read("TrainingImageLabel\Trainner.yml")
     else:
-        mess._show(title="Data Mining", message="Please click on Save Profile to reset data!")
+        mess._show(title='Data Missing', message='Please click on Save Profile to reset data!!')
         return
-    harcascadePath="haarcascade_frontalface_default.xml"
-    faceCascade=cv2.CascadeClassifier(harcascadePath)
-    
-    cam=cv2.VideoCapture(0)
-    font=cv2.FONT_HERSHEY_SIMPLEX
-    col_names=['Id', '','Name','','Date','','Time']
-    exists1=os.path.isfile("StudentDetails/StudentDetails.csv")
+    harcascadePath = "haarcascade_frontalface_default.xml"
+    faceCascade = cv2.CascadeClassifier(harcascadePath);
+
+    cam = cv2.VideoCapture(0)
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    col_names = ['Id', '', 'Name', '', 'Date', '', 'Time']
+    exists1 = os.path.isfile("StudentDetails\StudentDetails.csv")
     if exists1:
-        df=pd.read_csv("StudentDetails/StudentDetails.csv")
+        df = pd.read_csv("StudentDetails\StudentDetails.csv")
     else:
-        mess._show(title='Details Missing', message="Student Details are missing, Please Check!")
+        mess._show(title='Details Missing', message='Students details are missing, please check!')
         cam.release()
         cv2.destroyAllWindows()
         window.destroy()
-    while true:
-        ret,im= cam.read()
-        gray=cv2.cvtColor(im,cv2.COLOR_BGR2GREY)
-        faces= faceCascade.detectMultiScale(gray,1.2,5)
+    while True:
+        ret, im = cam.read()
+        gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+        faces = faceCascade.detectMultiScale(gray, 1.2, 5)
         for (x, y, w, h) in faces:
-            cv2.rectangle(im, (x, y), (x + w, y + h), (255, 0, 0), 2)
-            serial,conf=recognizer.predict(gray[y:y+h,x:x+w])
-            if(conf<50):
-                ts=time.time()
-                date=datetime.datetime.fromtimestamp(ts).strftime('%d-%m-%Y')
-                timeStamp=datetime.datetime.fromtimestamp(ts).strftime('%H:%M:$S')
-                a=df.loc[df['SERIAL NO.']==serial]['NAME'].values
-                ID=df.loc[df['SERIAL NO.']==serial]['ID'].values
-                ID=str(ID)
-                ID=ID[1:-1]
-                b=str(a)
-                b=b[2:-2]
-                attendance=[str(ID),'',b,'',str(date),'',str(timeStamp)]
+            cv2.rectangle(im, (x, y), (x + w, y + h), (225, 0, 0), 2)
+            serial, conf = recognizer.predict(gray[y:y + h, x:x + w])
+            if (conf < 50):
+                ts = time.time()
+                date = datetime.datetime.fromtimestamp(ts).strftime('%d-%m-%Y')
+                timeStamp = datetime.datetime.fromtimestamp(ts).strftime('%H:%M:%S')
+                aa = df.loc[df['SERIAL NO.'] == serial]['NAME'].values
+                ID = df.loc[df['SERIAL NO.'] == serial]['ID'].values
+                ID = str(ID)
+                ID = ID[1:-1]
+                bb = str(aa)
+                bb = bb[2:-2]
+                attendance = [str(ID), '', bb, '', str(date), '', str(timeStamp)]
+
             else:
-                Id='Unknown'
-                b=str(Id)
-                cv2.putText(im,str(b),(x,y+h),font,1,(255,255,255),2)
-            cv2.imshow('Taling Attendance',im)
-            if(cv2.waitKey(1)==ord('q')):
-                break
-            ts=time.time()
-            date=datetime.datetime.fromtimestamp(ts).strftime('%d-%m-%Y')
-            exists=os.path.isfile("Attendance/Attendance_"+date+".csv")
-            if exists:
-                with open("Attendance/Attendance_"+date+".csv",'a+') as csvFile1:
-                    writer=csv.writer(csvFile1)
-                    writer.writerow(col_names)
-                    writer.writerow(attendance)
-                csvFile1.close()
-            with open("Attendance/Attendance_"+date+".csv",'r') as csvFile1:
-                reader1=csv.reader(csvFile1)
-                for lines in reader1:
-                    i=i+1
-                    if(i>1):
-                        if(i%2!=0):
-                            iidd=str(lines[0])+' '
-                            tv.insert('',0,text=iidd,values=(str(lines[2]),str(lines[4]),str(lines[6])))
-            csvFile1.close()
-            cam.release()
-            cv2.destroyAllWindows()    
-            
-            
-        
+                Id = 'Unknown'
+                bb = str(Id)
+            cv2.putText(im, str(bb), (x, y + h), font, 1, (255, 255, 255), 2)
+        cv2.imshow('Taking Attendance', im)
+        if (cv2.waitKey(1) == ord('q')):
+            break
+    ts = time.time()
+    date = datetime.datetime.fromtimestamp(ts).strftime('%d-%m-%Y')
+    exists = os.path.isfile("Attendance\Attendance_" + date + ".csv")
+    if exists:
+        with open("Attendance\Attendance_" + date + ".csv", 'a+') as csvFile1:
+            writer = csv.writer(csvFile1)
+            writer.writerow(attendance)
+        csvFile1.close()
+    else:
+        with open("Attendance\Attendance_" + date + ".csv", 'a+') as csvFile1:
+            writer = csv.writer(csvFile1)
+            writer.writerow(col_names)
+            writer.writerow(attendance)
+        csvFile1.close()
+    with open("Attendance\Attendance_" + date + ".csv", 'r') as csvFile1:
+        reader1 = csv.reader(csvFile1)
+        for lines in reader1:
+            i = i + 1
+            if (i > 1):
+                if (i % 2 != 0):
+                    iidd = str(lines[0]) + '   '
+                    tv.insert('', 0, text=iidd, values=(str(lines[2]), str(lines[4]), str(lines[6])))
+    csvFile1.close()
+    cam.release()
+    cv2.destroyAllWindows() 
     
 #Let's create date and time
 global key
